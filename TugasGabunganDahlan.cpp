9cce43a344c290c7310db739bdb93699b1c52ff9
AhmadDahlan <ahmddhln197@gmail.com>
2017-11-19 19:34:20 +0700

    Input untuk memilih suatu perhitungan yang di inginkan

--- TugasGabunganDahlan.cpp
+++ TugasGabunganDahlan.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
+	int panjang, lebar, luas ,pilihan, alas, tinggi;
+	int gpkk,jk;
+	int lbr,gt,pjk,gk;
+	int w,X,y;
+	int x;
+	double Tinggi, berat, p,l,a,t,L;
+	char yt, bidang;
+	atas:
 	
+	cout <<"1.Perhitungan Persegi Panjang Dan Segitiga\n";
+	cout <<"2.Perhitungan Absolute\n";
+	cout <<"3.Perhitungan Berat Badan\n";
+	cout <<"4.Penentuan Nilai\n";
+	cout <<"5.Perhitungan Gaji Karyawan\n";
+	cout <<"Masukan Pilihan Hitungan Yang Anda Inginkan =";cin >>pilihan;
 }
